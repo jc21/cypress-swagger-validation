45d0a4631adc62c9c9eec8d5d5d2453a5777f9b8
Jamie Curnow <jc@jc21.com>
2023-10-17 23:36:03 +1000

    Update deps, replace chalk with pico

--- src/logger.ts
+++ src/logger.ts
@@ -1,4 +1,4 @@
-import * as chalk from 'chalk';
+import * as pc from "picocolors"
 import * as _ from 'lodash';
 
 export default class Logger {
@@ -11,33 +11,33 @@ export default class Logger {
 
 	public success(...args: any[]) {
 		const arr: any[] = _.values(args);
-		arr.unshift(this.getTitle(chalk.green.bold('SUCCESS')));
+		arr.unshift(this.getTitle('SUCCESS', pc.green));
 		// @ts-ignore
 		console.log.apply(null, arr);
 	}
 
 	public error(...args: any[]) {
 		const arr: any[] = _.values(args);
-		arr.unshift(this.getTitle(chalk.red.bold('ERROR')));
+		arr.unshift(this.getTitle('ERROR', pc.red));
 		// @ts-ignore
 		console.log.apply(null, arr);
 	}
 
 	public info(...args: any[]) {
 		const arr: any[] = _.values(args);
-		arr.unshift(this.getTitle(chalk.blue.bold('INFO')));
+		arr.unshift(this.getTitle('INFO', pc.blue));
 		// @ts-ignore
 		console.log.apply(null, arr);
 	}
 
 	public debug(...args: any[]) {
 		const arr: any[] = _.values(args);
-		arr.unshift(this.getTitle(chalk.magenta.bold('DEBUG')));
+		arr.unshift(this.getTitle('DEBUG', pc.magenta));
 		// @ts-ignore
 		console.log.apply(null, arr);
 	}
 
-	private getTitle(type: string): string {
-		return chalk.blue.bold('[') + chalk.cyan.bold(this.title) + ' ' + type + chalk.blue.bold(']');
+	private getTitle(type: string, colorFn: any): string {
+		return pc.blue('[') + pc.cyan(this.title) + ' ' + colorFn(pc.bold(type)) + pc.blue(']');
 	}
 }
